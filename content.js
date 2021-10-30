e9a19764cf6ccf51a2ba7aedcb3943dca070bbab
uongsuadaubung <manhkien13041997@gmail.com>
2021-10-31 00:37:34 +0700

    update

--- content.js
+++ content.js
@@ -1,72 +1,77 @@
-let stringAds = [
-    "Post you may like",
-    "Suggested for you",
-    "Sponsored",
-    "Videos just for you",
-    "Your saved videos on Facebook Watch",
-    "People You May Know",
-    "Gợi ý cho bạn"
-]
-// chrome.storage.sync.set({'string_ads': stringAds})
-let isRuning = false
-let meow = []
-let maxPost = 30
-let most_recent
-chrome.storage.sync.get(['most_recent', 'limit','string_ads'], (items) => {
-    // alert(JSON.stringify(items))
-    most_recent = items.most_recent ?? true
-    maxPost = items.limit ?? 30
+(async ()=>{
+    let load = async (key)=>{
+        return await new Promise(resolve => {
+            chrome.storage.sync.get([key], item =>{
+                resolve(item[key])
+            })
+        })
+    }
+    let most_recent = (await load('most_recent')) ?? true
     if (most_recent === true && location.pathname === '/' && !location.search) {
         location.replace('https://www.facebook.com/?sk=h_chr')
     }
-})
+    //////////////////////////////////////////////////////////////////////////////
+    let stringAds = (await load('string_ads')) ?? []
+    let isRuning = false
+    let meow = [] // total posts, btw i love cats
+    let maxPost = (await load('amount')) ?? 30
 
-window.onscroll = function () {
-    // called when the window is scrolled.
-    this.removeAds()
-}
-removeAds = () => {
-    if (!isRuning) {
-        isRuning = true
-        if (location.pathname === '/watch/') {
-            meow = document.querySelectorAll('[class="j83agx80 cbu4d94t"]')
-        } else if (location.pathname === '/') {
-            //đây là trang chủ
-            meow = document.querySelectorAll("[data-pagelet]");
-        }
-        meow.forEach((ele) => {
-            Promise.all(stringAds.map(ads => {
-                if (ele && ele.innerText.indexOf(ads) !== -1) {
-                    ele.remove()
-                    console.log("Meow mewo đã xoá quảng cáo")
+    let limit_post = (await load('limit_post')) ?? true
+    let remove_post = (await load('remove_post')) ?? true
+
+    window.onscroll = function () {
+        // called when the window is scrolled.
+        removeAds()
+    }
+    let removeAds = () => {
+        if (!isRuning && remove_post) {
+            isRuning = true
+            if (location.pathname === '/watch/') {
+                meow = document.querySelectorAll('div[class="j83agx80 cbu4d94t"]')
+            } else if (location.pathname === '/') {
+                //đây là trang chủ
+                meow = document.querySelectorAll('div[data-pagelet^="FeedUnit_"]');
+            }
+            meow.forEach((ele) => {
+                Promise.all(stringAds.map(ads => { // mặc dù dùng loop thông thường mất khoảng hơn 100ms và promiseall mất có 1ms tuy đều không thể nhận ra bằng mắt nhưng thôi ksao
+                    if (ele && ele.innerText.indexOf(ads) !== -1) {
+                        ele.remove()
+                        console.log("Meow meow đã xoá quảng cáo")
+                    }
+                })).then()
+            })
+            if (limit_post) {
+                if (meow.length > maxPost) {
+                    let dif = meow.length - maxPost;
+                    for (let i = 0; i < dif; i++) {
+                        meow[i].remove()
+                    }
                 }
-            }))
-        })
-        if (meow.length > maxPost) {
-            let dif = meow.length - maxPost;
-            for (let i = 0; i < dif; i++) {
-                meow[i].remove()
             }
+
+            isRuning = false
         }
-        isRuning = false
+
     }
 
-}
-
-changeLink = () => {
-    if (most_recent) {
-        let atags = document.getElementsByTagName('a')
-        for (const a of atags) {
-            if (a.href === 'https://www.facebook.com/') {
-                a.href = 'https://www.facebook.com/?sk=h_chr'
-                a.onclick = () => { location.replace('https://www.facebook.com/?sk=h_chr') }
+    let changeLink = () => {
+        if (most_recent) {
+            let atags = document.getElementsByTagName('a')
+            for (const a of atags) {
+                if (a.href === 'https://www.facebook.com/') {
+                    a.href = 'https://www.facebook.com/?sk=h_chr'
+                    a.onclick = () => {
+                        location.replace('https://www.facebook.com/?sk=h_chr')
+                    }
+                }
             }
         }
     }
-}
-window.onload = () => {
-    document.title = document.title.replaceAll("Facebook", "Hạnh Xấu Xí")
-    changeLink()
-    removeAds()
-}
+    window.onload = () => {
+        document.title = document.title.replaceAll("Facebook", "Hạnh Xấu Xí")
+        changeLink()
+        removeAds()
+    }
 
+
+})()
--- popup.js
+++ popup.js
@@ -1,42 +1,96 @@
-window.onload = function() {
+window.onload = async function() {
     let most_recent_ele = document.getElementById("most_recent")
-    let limit_ele = document.getElementById("limit")
-    chrome.storage.sync.get(['most_recent','limit','string_ads'], (items)=>{ 
-        let checked = items.most_recent
-        let limit = items.limit
-        // let ads = items.string_ads
-        //1st time init
-        if (checked === undefined || limit === undefined) {
-            chrome.storage.sync.set({'most_recent': true})
-            checked = true
-            chrome.storage.sync.set({'limit': 30})
-            limit = 30
-        }
-        if (checked) {
-            most_recent_ele.checked = true
-        }
-        if (limit) {
-            limit_ele.value = limit
-        }
-        // if (!ads) {
-        //     ads = [
-        //         "Post you may like",
-        //         "Suggested for you",
-        //         "Sponsored",
-        //         "Videos just for you",
-        //         "Your saved videos on Facebook Watch",
-        //         "People You May Know",
-        //         "Gợi ý cho bạn"
-        //     ]
-        // }else {
-            
-        // }
-     });
-    
+    let limit_amount_ele = document.getElementById("limit_amount")
+    let limit_post_ele = document.getElementById("limit_post")
+    let remove_post_ele = document.getElementById("remove_post")
+    let save = function (key, value) {
+        let data= {}
+        data[key] = value
+        chrome.storage.sync.set(data)
+    }
+    let load = async (key)=>{
+        return await new Promise(resolve => {
+            chrome.storage.sync.get([key], item =>{
+                resolve(item[key])
+            })
+        })
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////
+    let isMostRecent = await load('most_recent')
+    if (isMostRecent === undefined){
+        save('most_recent', true)
+        isMostRecent = true
+    }
+    if (isMostRecent) {
+        most_recent_ele.checked = true
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    let isLimitPost = await load('limit_post')
+    if (isLimitPost === undefined){
+        save('limit_post', true)
+        isLimitPost = true
+    }
+    if (isLimitPost){
+        limit_post_ele.checked = true
+        document.getElementById("group_limit_amount").classList.remove("d-none");
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    let amount = await load('amount')
+    if ( amount === undefined){
+        save('amount', 30)
+        amount = 30
+    }
+    if (amount) {
+        limit_amount_ele.value = amount
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    let isRemovePost = await load('remove_post')
+    if (isRemovePost === undefined) {
+        save('remove_post', true)
+        isRemovePost = true
+    }
+    if (isRemovePost){
+        remove_post_ele.checked = true
+        document.getElementById("group_keywords").classList.remove("d-none");
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    let string_ads = await load('string_ads')
+    if (string_ads === undefined){
+        string_ads =
+            [
+                "Post you may like",
+                "Suggested for you",
+                "Sponsored",
+                "Videos just for you",
+                "Your saved videos on Facebook Watch",
+                "People You May Know",
+                "Gợi ý cho bạn"
+            ]
+        save('string_ads', string_ads)
+    }
+
     most_recent_ele.addEventListener("change", function() {
-        chrome.storage.sync.set({'most_recent': most_recent.checked})
+        save('most_recent', most_recent.checked)
     })
-    limit_ele.addEventListener("change", function() {
-        chrome.storage.sync.set({'limit': Number(limit_ele.value)})
+    limit_amount_ele.addEventListener("change", function() {
+        save('amount', Number(limit_amount_ele.value))
+    })
+    limit_post_ele.addEventListener("change", function() {
+        let res = limit_post_ele.checked
+        if (res){
+            document.getElementById("group_limit_amount").classList.remove("d-none");
+        }else {
+            document.getElementById("group_limit_amount").classList.add("d-none");
+        }
+        save('limit_post', res)
+    })
+    remove_post_ele.addEventListener("change", function() {
+        let res = remove_post_ele.checked
+        if (res){
+            document.getElementById("group_keywords").classList.remove("d-none");
+        }else {
+            document.getElementById("group_keywords").classList.add("d-none");
+        }
+        save('remove_post', res)
     })
 }

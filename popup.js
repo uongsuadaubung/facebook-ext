bed93a108391a248ac308a87df283be1c6d82227
uongsuadaubung <manhkien13041997@gmail.com>
2021-10-31 10:52:51 +0700

    update

--- popup.js
+++ popup.js
@@ -3,6 +3,8 @@ window.onload = async function() {
     let limit_amount_ele = document.getElementById("limit_amount")
     let limit_post_ele = document.getElementById("limit_post")
     let remove_post_ele = document.getElementById("remove_post")
+    let btn_add_ele = document.getElementById("btn_add")
+
     let save = function (key, value) {
         let data= {}
         data[key] = value
@@ -15,6 +17,7 @@ window.onload = async function() {
             })
         })
     }
+
     ///////////////////////////////////////////////////////////////////////////////////////
     let isMostRecent = await load('most_recent')
     if (isMostRecent === undefined){
@@ -69,6 +72,36 @@ window.onload = async function() {
         save('string_ads', string_ads)
     }
 
+    let addToTable = (value, method)=>{
+        const table_ads_ele = document.getElementById("table_ads")
+        const rowCount = table_ads_ele.rows.length;
+        const row = table_ads_ele.insertRow(rowCount);
+        row.id = 'row_' + rowCount
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const deletebtn = document.createElement("button")
+        deletebtn.type = "button"
+        deletebtn.innerText = "Xoá"
+        deletebtn.className = "btn btn-danger btn-sm"
+        deletebtn.onclick = ()=>{
+            let index = string_ads.findIndex(i=>i===value)
+            if (index!== -1){
+                string_ads.splice(index,1)
+                let row = document.getElementById('row_'+rowCount)
+                row.parentNode.removeChild(row)
+                save('string_ads', string_ads)
+            }
+        }
+        if (method === 'add'){ // add || load
+            string_ads.push(value)
+            save('string_ads', string_ads)
+        }
+        cell1.innerHTML = value;
+        cell2.appendChild(deletebtn)
+    }
+    for (const stringAd of string_ads) {
+        addToTable(stringAd, 'load')
+    }
     most_recent_ele.addEventListener("change", function() {
         save('most_recent', most_recent_ele.checked)
     })
@@ -93,4 +126,27 @@ window.onload = async function() {
         }
         save('remove_post', res)
     })
+    btn_add_ele.addEventListener('click', ()=>{
+        let inputkeyword = document.getElementById('inputkeyword')
+        let input = inputkeyword.value
+        if (string_ads.findIndex(i=>i===input)!==-1){
+            alertManager('Keyword đã tồn tại')
+            return
+        }
+        if (input){
+            addToTable(input, 'add')
+            inputkeyword.value = ''
+        }else {
+            alertManager('Keyword phải chứa ký tự')
+        }
+
+    })
+    let alertManager = (message)=>{
+        let alert = document.getElementById('alert_danger')
+        alert.classList.remove('d-none')
+        alert.innerText = message
+        setTimeout(()=>{
+            alert.classList.add('d-none')
+        },2000)
+    }
 }

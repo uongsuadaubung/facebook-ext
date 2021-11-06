68914c1737b548f708e1100ac5f3d46a3ae6df39
uongsuadaubung <manhkien13041997@gmail.com>
2021-11-06 22:00:11 +0700

    update

--- background.js
+++ background.js
@@ -1,24 +1,20 @@
 (async ()=>{
     let freezeNewsFeed = (await load('freeze_newsfeed')) ?? false
-    let isAllowBlockOtherRequest = (await load('block_other_request')) ?? false
-    let blockUrls = (await load('block_urls')) ?? ["nw/","api/graphql/"]
+    let blockUrls = (await load('block_urls')) ?? []
+    let freezeNewsFeedUrls = ["https://www.facebook.com/nw/","https://www.facebook.com/api/graphql/"]
     //TODO: về sau làm nốt mục block other request
-    if (freezeNewsFeed){
-        chrome['webRequest']['onBeforeRequest']['addListener'](
-            () => {return {cancel: true}},
-            {urls: ["https://www.facebook.com/nw/","https://www.facebook.com/api/graphql/"]},
-            ["blocking"]
-        );
-    }
-    if (isAllowBlockOtherRequest){
-        chrome['webRequest']['onBeforeRequest']['addListener'](
-            details => {
-                return {cancel: blockUrls.some(keyword => details.url.includes(keyword))};
-            },
-            {urls: ["<all_urls>"]},
-            ["blocking"]
-        );
-    }
+
+    chrome['webRequest']['onBeforeRequest']['addListener'](
+        details => {
+            if (freezeNewsFeed && freezeNewsFeedUrls.some(keyword => details.url.includes(keyword))){
+                return {cancel: true}
+            }
+            return {cancel: blockUrls.some(keyword => details.url.includes(keyword))};
+        },
+        {urls: ["<all_urls>"]},
+        ["blocking"]
+    );
+
     onMessage['addListener'](message=>{
         if (message['todo'] === "show"){
             chrome['tabs']['query']({active:true, currentWindow:true}, tabs=>{

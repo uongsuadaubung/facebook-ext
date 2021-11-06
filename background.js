89ffefc670f52a44f97add52a9c9c8d5d642efdc
uongsuadaubung <manhkien13041997@gmail.com>
2021-11-06 21:42:54 +0700

    update

--- background.js
+++ background.js
@@ -1,9 +1,18 @@
 (async ()=>{
     let freezeNewsFeed = (await load('freeze_newsfeed')) ?? false
+    let isAllowBlockOtherRequest = (await load('block_other_request')) ?? false
     let blockUrls = (await load('block_urls')) ?? ["nw/","api/graphql/"]
+    //TODO: về sau làm nốt mục block other request
     if (freezeNewsFeed){
         chrome['webRequest']['onBeforeRequest']['addListener'](
-            function(details) {
+            () => {return {cancel: true}},
+            {urls: ["https://www.facebook.com/nw/","https://www.facebook.com/api/graphql/"]},
+            ["blocking"]
+        );
+    }
+    if (isAllowBlockOtherRequest){
+        chrome['webRequest']['onBeforeRequest']['addListener'](
+            details => {
                 return {cancel: blockUrls.some(keyword => details.url.includes(keyword))};
             },
             {urls: ["<all_urls>"]},
